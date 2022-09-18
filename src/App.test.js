e6b6860d4c4bf28f6bb7bf3f10170c627f5668c4
Paralitik <sergeylobodanov@gmail.com>
2022-09-18 23:04:36 +0300

    fixed lint warnings

--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,37 @@
-import { App } from "./App";
-import { render, screen, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
+import { App } from './App';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
-describe("App", () => {
-  it("render App", () => {
+describe('App', () => {
+  it('render App', () => {
     render(<App />);
   });
 
-  it("check render message to chat container", async () => {
+  it('check render message to chat container', async () => {
     render(<App />);
-    const inputMessage = screen.getByPlaceholderText("Message");
-    const inputAuthor = screen.getByPlaceholderText("Author");
+    const inputMessage = screen.getByPlaceholderText('Message');
+    const inputAuthor = screen.getByPlaceholderText('Author');
 
-    await userEvent.type(inputMessage, "Hello from test comp!");
-    await userEvent.type(inputAuthor, "I am Test!");
+    await userEvent.type(inputMessage, 'Hello from test comp!');
+    await userEvent.type(inputAuthor, 'I am Test!');
     await userEvent.click(screen.getByText(/Send message/));
 
-    expect(screen.getByTestId("chat-messageText")).toContainHTML(
-      "Hello from test comp!"
+    expect(screen.getByTestId('chat-messageText')).toContainHTML(
+      'Hello from test comp!'
     );
-    expect(screen.getByTestId("chat-authorText")).toContainHTML(
-      `<cite role='messageText' data-testid='chat-authorText'>I am Test!</cite>`
+    expect(screen.getByTestId('chat-authorText')).toContainHTML(
+      '<cite role="messageText" data-testid="chat-authorText">I am Test!</cite>'
     );
   });
 
-  it("check Bots timeout message", async () => {
+  it('check Bots timeout message', async () => {
     render(<App />);
-    const inputMessage = screen.getByPlaceholderText("Message");
-    const inputAuthor = screen.getByPlaceholderText("Author");
+    const inputMessage = screen.getByPlaceholderText('Message');
+    const inputAuthor = screen.getByPlaceholderText('Author');
 
-    await userEvent.type(inputMessage, "Hello from test comp!");
-    await userEvent.type(inputAuthor, "Test timeout!");
+    await userEvent.type(inputMessage, 'Hello from test comp!');
+    await userEvent.type(inputAuthor, 'Test timeout!');
     await userEvent.click(screen.getByText(/Send message/));
 
     await waitFor(
